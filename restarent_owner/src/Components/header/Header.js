5b896077c9acd992cbf079ff9cdb8e5717b92979
Sai Krishna <g.saikrishna20011@gmail.com>
2023-10-31 21:13:22 +0530

    first commit

---
+++ restarent_owner/src/Components/header/Header.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import '../../styles/header.css'
+
+const Header = () => {
+    return (
+
+        <div class="sc-jKJlTe ddPhHm">
+            <div class="sc-1mo3ldo-0 sc-etwtAo iGlvVK">
+                <div height="600px" width="100%" class="s1isp7-0 fAEvcR sc-jXQZqI ihibkK">
+                    <div src="" class="s1isp7-2 dBjfsr">
+                    </div>
+                    <img alt="web-backdrop" src="https://b.zmtcdn.com/mx-onboarding-hero87f77501659a5656cad54d98e72bf0d81627911821.webp" loading="lazy" class="s1isp7-4 bBALuk" />
+                </div>
+                <div class="sc-iGPElx elZPjT">
+                </div>
+            </div>
+            <div width="100%" class="sc-iyvyFf sc-kasBVs gSTorL">
+                <div class="sc-iyvyFf eboBpz">
+                    <h1 color="#fff" font-size="18px,,24px,36px" font-weight="400,,,500" class="sc-iyvyFf ksPCsP">
+                        Partner with Zomato</h1>
+                    <h1 color="#fff" font-size="18px,,24px,36px" font-weight="400,,,500" class="sc-iyvyFf dufcds">
+                        at 0% commission for the 1st month!</h1>
+                    <div color="#fff" font-size="14px,,16px,16px" font-weight="400" class="sc-iyvyFf hywdtO">
+                        And get ads worth INR 1500. Valid for new restaurant partners in select cities.</div>
+                </div>
+                <section class="sc-iwsKbI Sbsqo">
+                    <div class="sc-iyvyFf gAIjKt">
+                        <div width="100%" display="flex" class="sc-iyvyFf fCbBgL">
+                            <div display="flex" class="sc-iyvyFf hzbHFu">
+                                <button id="diy-register-your-res" font-size="17px" class="sc-1kx5g6g-1 cTRhrB sc-kZmsYB bSbOrh" role="button" tabindex="0" aria-disabled="false">
+                                    <span id="diy-register-your-res" font-size="17px" tabindex="-1" class="sc-1kx5g6g-2 htnRwj">
+                                        <span class="sc-1kx5g6g-3 dkwpEa">
+                                            Register your restaurant</span>
+                                    </span>
+                                </button>
+                            </div>
+                            <div display="flex" class="sc-iyvyFf gPYJfL">
+                                <button id="diy-claim-now" color="black" font-size="17px" class="sc-1kx5g6g-1 cTRhrB sc-kZmsYB fxSGYu" role="button" tabindex="0" aria-disabled="false">
+                                    <span id="diy-claim-now" color="black" font-size="17px" tabindex="-1" class="sc-1kx5g6g-2 fKpBNQ">
+                                        <span class="sc-1kx5g6g-3 dkwpEa">
+                                            Login to view your existing restaurants</span>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div color="#fff" font-size="12px,,14px" class="sc-iyvyFf hRUDEe">
+                        Need help? Please email us at <a color="#fff" href="mailto:merchantonboarding@zomato.com?subject=Need help with restaurant onboarding on Zomato" class="sc-jtggT gBmCeh">
+                            merchantonboarding@zomato.com</a>
+                    </div>
+                    <div width="100%" class="sc-iyvyFf mNsJs" style={{"top": "178px"}}>
+                        <div class="sc-iyvyFf hULTtM">
+                            <h1 font-size="30px" font-weight="500" color="#363636" class="sc-iyvyFf eoXGbe">
+                                Get started with online ordering</h1>
+                            <h3 font-size="16px" font-weight="400" color="#696969" class="sc-iyvyFf fIXayO">
+                                Please keep the documents ready for a smooth signup</h3>
+                            <div display="flex" class="sc-iyvyFf fTSjwy">
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            FSSAI license copy</span>
+                                    </div>
+                                </div>
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            PAN card copy</span>
+                                    </div>
+                                </div>
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            Regular GSTIN (if applicable)</span>
+                                    </div>
+                                </div>
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            Bank account details</span>
+                                    </div>
+                                </div>
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            Your restaurant menu</span>
+                                    </div>
+                                </div>
+                                <div display="flex" class="sc-iyvyFf burCAT">
+                                    <div class="sc-iyvyFf bBkFUI">
+                                        <span unicode="E93B" color="#3ab757" class="sc-gzOgki hyJjHU">
+                                        </span>
+                                        <span font-weight="500" color="#363636" class="sc-iyvyFf KXwWu">
+                                            Dish images for top 5 items</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Header
